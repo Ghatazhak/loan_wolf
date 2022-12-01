3381d03384c2247ceabb7371dac7e2d9bc7f9f6e
Bart Lyons <lyons.bart@protonmail.com>
2022-12-01 00:46:10 -0500

    Done for tonight

--- js/site.js
+++ js/site.js
@@ -1,4 +1,42 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+
+function getValues(){
+    let loanAmount = document.getElementById("loanAmount").value;
+    let term = document.getElementById("term").value;
+    let interestRate = document.getElementById("interestRate").value;
+
+    let paymentAmount = loanAmount * (interestRate/1200)/(1-Math.pow( 1 + interestRate / 1200,-term));
+
+
+    document.getElementById("totalPrincipal").innerHTML = loanAmount;
+    document.getElementById("monthlyPayments").innerHTML = Math.round(paymentAmount);
+
+    calculate(loanAmount, term, interestRate);
+}
+
+
+
+function calculate (loanAmount, term, interestRate){
+    
+    
+
+   
+    let RemainingBalance = loanAmount  ;
+    let PreviousInterestPayment = 0;
+    let PreviousPrincipalPayment = 0;
+
+
+    for (let index = 1; index <= term; index++) {
+        
+        
+        
+    }
+
+
+
+}
+
+
+
+function displayResults(){
+
 }
